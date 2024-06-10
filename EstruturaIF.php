976e34288bea2d252932eebca994bc95eef69973
Pedrinh0O <165160365+Pedrinh0O@users.noreply.github.com>
2024-06-10 08:36:15 -0300

    Add files via upload

---
+++ EstruturaIF.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <body>
+
+
+    <?php
+$idade=20;
+
+if($idade > 18){
+    echo "Você é maior de idade";
+}
+?>
+    </body>
+</html>
+    
+
+
+
+
---
+++ Estruturadecisãoelsenome.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <?php
+  $nome = 'Till Lindemann';
+  if($nome == 'Richard Kruspe') {
+    echo 'E ae Richard Kruspe!';
+
+  } elseif ($nome == 'Oliver Tree'){
+  echo 'E ae Oliver Tree';
+  } elseif ($nome == 'Till Lindemann') {
+    echo 'E ae Till Lindemann';
+  }else {
+    echo 'E ae $nome!';
+
+  }
+?>
+</body>
+</html>
